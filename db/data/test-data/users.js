217398986f7d6041ec1b2b22ee397743f33f7518
Callum Blejean <callumblejean@gmail.com>
2024-11-18 22:35:38 +0000

    removed falase key value pair of author from users db

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -2,35 +2,30 @@ module.exports = [
   {
     name: "Abdiaziz",
     email: "abdiaziz@northcoders.co.uk",
-    author: "butter_bridge",
     avatar_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
   {
     name: "Jasmine",
     email: "jasmine@northcoders.co.uk",
-    author: "jasmine_guru",
     avatar_url:
       "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?w=700&h=700",
   },
   {
     name: "Liam",
     email: "liam@northcoders.co.uk",
-    author: "liam_rocks",
     avatar_url:
       "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=700&h=700",
   },
   {
     name: "Sophia",
     email: "sophia@northcoders.co.uk",
-    author: "sophia_codes",
     avatar_url:
       "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?w=700&h=700",
   },
   {
     name: "Ethan",
     email: "ethan@northcoders.co.uk",
-    author: "ethan_hacks",
     avatar_url:
       "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?w=700&h=700",
   },
