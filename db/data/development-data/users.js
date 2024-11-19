d88d1ecd32cd89f179fa5ef6a9ca182ff226b14f
Callum Blejean <callumblejean@gmail.com>
2024-11-19 08:18:54 +0000

    corrected incorrect format in user dev db and added correct created by key to both user dbs

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,36 +2,36 @@ module.exports = [
   {
     name: "Abdiaziz",
     email: "abdiaziz@northcoders.co.uk",
-    author: "butter_bridge",
     avatar_url:
-      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-01T12:00:00.000Z",
   },
   {
     name: "Jasmine",
     email: "jasmine@northcoders.co.uk",
-    author: "jasmine_guru",
     avatar_url:
-      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-02T12:00:00.000Z",
   },
   {
     name: "Liam",
     email: "liam@northcoders.co.uk",
-    author: "liam_rocks",
     avatar_url:
-      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-03T12:00:00.000Z",
   },
   {
     name: "Sophia",
     email: "sophia@northcoders.co.uk",
-    author: "sophia_codes",
     avatar_url:
-      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-04T12:00:00.000Z",
   },
   {
     name: "Ethan",
     email: "ethan@northcoders.co.uk",
-    author: "ethan_hacks",
     avatar_url:
-      "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-05T12:00:00.000Z",
   },
 ];
--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -3,30 +3,35 @@ module.exports = [
     name: "Abdiaziz",
     email: "abdiaziz@northcoders.co.uk",
     avatar_url:
-      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-01T12:00:00.000Z",
   },
   {
     name: "Jasmine",
     email: "jasmine@northcoders.co.uk",
     avatar_url:
-      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-02T12:00:00.000Z",
   },
   {
     name: "Liam",
     email: "liam@northcoders.co.uk",
     avatar_url:
-      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-03T12:00:00.000Z",
   },
   {
     name: "Sophia",
     email: "sophia@northcoders.co.uk",
     avatar_url:
-      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-04T12:00:00.000Z",
   },
   {
     name: "Ethan",
     email: "ethan@northcoders.co.uk",
     avatar_url:
-      "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?w=700&h=700",
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: "2024-01-05T12:00:00.000Z",
   },
 ];
