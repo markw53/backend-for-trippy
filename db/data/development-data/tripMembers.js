4a1c67ac78fa6b3e4e3122eba3cdec78f74a2d3d
Callum Blejean <callumblejean@gmail.com>
2024-11-22 10:00:51 +0000

    expanded db and hardcoded guest user

--- db/data/development-data/tripMembers.js
+++ db/data/development-data/tripMembers.js
@@ -24,4 +24,24 @@ module.exports = [
     user_id: 5,
     is_admin: true,
   },
+  {
+    trip_id: 1,
+    user_id: 6,
+    is_admin: true,
+  },
+  {
+    trip_id: 3,
+    user_id: 6,
+    is_admin: false,
+  },
+  {
+    trip_id: 2,
+    user_id: 6,
+    is_admin: true,
+  },
+  {
+    trip_id: 4,
+    user_id: 6,
+    is_admin: false,
+  },
 ];
