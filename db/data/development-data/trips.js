6ae88a19c48a3386b7b2e16585a7a47c32a8fe7e
Callum Blejean <callumblejean@gmail.com>
2024-11-25 09:27:16 +0000

    added user id to trip data

--- db/data/development-data/trips.js
+++ db/data/development-data/trips.js
@@ -4,6 +4,7 @@ module.exports = [
     location: "Barbados",
     description: "Silent Assassin",
     start_date: "2022-02-02",
+    created_by: 6,
     trip_img_url:
       "https://static.wikia.nocookie.net/versus-compendium/images/1/13/Ac_2_ezio.png/revision/latest/scale-to-width-down/1000?cb=20200113175628",
   },
@@ -12,6 +13,7 @@ module.exports = [
     location: "Rome, Italy",
     description: "Explore the ancient ruins and vibrant culture.",
     start_date: "2023-05-10",
+    created_by: 6,
     trip_img_url:
       "https://images.pexels.com/photos/533848/pexels-photo-533848.jpeg?w=700&h=700",
   },
@@ -20,6 +22,7 @@ module.exports = [
     location: "Aspen, Colorado",
     description: "Hit the slopes with friends!",
     start_date: "2024-01-15",
+    created_by: 6,
     trip_img_url:
       "https://images.pexels.com/photos/90946/ski-skiing-winter-snow-90946.jpeg?w=700&h=700",
   },
@@ -28,6 +31,7 @@ module.exports = [
     location: "Tokyo, Japan",
     description: "Experience the blend of tradition and modernity.",
     start_date: "2023-11-20",
+    created_by: 6,
     trip_img_url:
       "https://images.pexels.com/photos/346768/pexels-photo-346768.jpeg?w=700&h=700",
   },
--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -34,4 +34,11 @@ module.exports = [
       "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     created_at: "2024-01-05T12:00:00.000Z",
   },
+  {
+    name: "Guest User",
+    email: "guest@example.com",
+    avatar_url:
+      "https://cdn.content.tuigroup.com/adamtui/2023_2/8_14/9be5b1f9-5df3-4e1a-9422-afa300f2f277/DOMINICAN_REPUBLIC_BRAND_23_F016_SAONAWebOriginalCompressed.jpg",
+    created_at: "2024-01-01T12:00:00.000Z",
+  },
 ];
